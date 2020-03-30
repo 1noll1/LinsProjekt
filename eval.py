51292b87969ad5cdaeb12a9c4c786c2b62dc48ec
Linnea Strand <gusstrlip@student.gu.se>
2020-03-30 21:40:07 +0200

    sort things

--- eval.py
+++ eval.py
@@ -6,51 +6,6 @@ import pickle
 import numpy as np
 from OrtLoader import OrtLoader
 
-dev = torch.device("cuda:{}".format(hash('gusstrlip') % 4) if torch.cuda.is_available() else "cpu")
-
-# class OrtLoader():
-#     def __init__(self, smaort, tatort, vocab, max_len, sammansattningar=None):
-#         self.max_len = max_len
-#         self.vocab = vocab
-#         char2int = {t:n for n, t in enumerate(vocab)}
-#         if sammansattningar == None:
-#             total = smaort + tatort
-#         if sammansattningar != None:
-#             total = sammansattningar
-#         self.len = len(total)
-
-#         def ort2int(total):
-#             encoded = []
-#             for i, ort in enumerate(total):
-#                 try:
-#                     intort = [char2int[t] for t in ort]
-#                     encoded.append(torch.Tensor(intort))
-#                 except:
-#                     pass
-#             return encoded
-
-#         encoded = ort2int(total)
-
-#         x_padded = []
-#         for ort in encoded:
-#             seq = np.zeros(self.max_len)
-#             i = 0
-#             for _int in ort:
-#                seq[i] = _int
-#                i += 1
-#                x_padded.append(np.copy(seq))
-
-#         self.input_size = len(x_padded[0])
-
-#         self.X_tensors = [torch.LongTensor(seq) for seq in x_padded]
-#         self.y_tensors = torch.LongTensor([0 if ort in smaort else 1 for ort in total])
-
-#     def __len__(self):
-#         return self.len
-
-#     def __getitem__(self, i):
-#         return self.X_tensors[i].to(dev), self.y_tensors[i].to(dev)
-
 def model_eval(model, test_loader):
     model.eval()
     true = []
@@ -58,12 +13,12 @@ def model_eval(model, test_loader):
     with torch.no_grad():
         print('Initialising evaluation')
         for ort, label in test_loader:
-                true.extend([label])
-                ort = ort.unsqueeze(0)
-                out = model(ort)
-                _, predicted = torch.max(out.data, 1)
-                predicted = predicted.to('cpu')
-                pred.extend(predicted)
+            true.extend([label])
+            ort = ort.unsqueeze(0)
+            out = model(ort)
+            _, predicted = torch.max(out.data, 1)
+            predicted = predicted.to('cpu')
+            pred.extend(predicted)
 
     a = 0
     for pred,label in zip(pred,true):
@@ -71,22 +26,26 @@ def model_eval(model, test_loader):
             a += 1
     print('Number of test samples:', len(test_loader))
     print('Accurate predictions: {}'.format(a))
-    print('Total accuracy: {} {}'.format((a/len(true)) * 100, '%'))
+    print('Total accuracy: {} {}'.format((a/len(test_loader)) * 100, '%'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Evaluate place name classifier.')
-    parser.add_argument('--dataset', type=str, default="default_dataset",
+    parser.add_argument('--dataset', type=str, default="datasets/default_dataset",
                     help='The name of the file containing the dataset.')
-    parser.add_argument('--modelfile', type=str, default="trained_model",
+    parser.add_argument('--modelfile', type=str, default="trained_models/trained_model",
                     help='The name of the file containing the model to be evaluated.')
 
     args = parser.parse_args()
 
-    print('Loading dataset from {}'.format(args.dataset))
+    dev = torch.device("cuda:{}".format(hash('gusstrlip') % 4) if torch.cuda.is_available() else "cpu")
+
     with open('smaort_test.pkl', 'rb') as f:
         smaort = pickle.load(f)
     with open('tatort_test.pkl', 'rb') as f:
         tatort = pickle.load(f)
+
+    # make sure to load the fasttext dataset if you load the fasttext model :)
+    print('Loading dataset from {}'.format(args.dataset))
     with open(args.dataset, 'rb') as f:
         dataset = pickle.load(f)
 
@@ -95,13 +54,13 @@ if __name__ == '__main__':
 
     print('Loading trained model')
     trained = torch.load(args.modelfile)
-    
-    if args.modelfile == 'trained_model':
-        test_data = OrtLoader(smaort, tatort, vocab, max_len, None)
-    elif args.modelfile == 'fastText_trained_model':
+
+    if args.modelfile == 'trained_models/trained_model':
+        test_data = OrtLoader(smaort, tatort, vocab, max_len, dev, orter=None)
+    elif args.modelfile == 'trained_models/fastText_trained_model':
         with open('sammansattningar.pkl', 'rb') as f:
             ss = pickle.load(f)
-        test_data = OrtLoader(smaort, tatort, vocab, max_len, ss.values())
+        test_data = OrtLoader(smaort, tatort, vocab, max_len, dev, orter=ss)
 
     test_loader = DataLoader(dataset=test_data, batch_size=1, shuffle=False, num_workers=0)
     model_eval(trained, test_data)
--- main.py
+++ main.py
@@ -21,76 +21,41 @@ def get_vocab(dataframe, column_name):
     print('Vocab size:', vocab_size)
     return vocab, vocab_size
 
-# class OrtLoader():
-#     def __init__(self, smaort, tatort, vocab, max_len):
-#         self.max_len = max_len
-#         self.vocab = vocab
-#         char2int = {t:n for n, t in enumerate(vocab)}
-#         total = smaort + tatort
-#         self.len = len(total)
-
-#         def ort2int(total):   
-#             encoded = []         
-#             for i, ort in enumerate(total):
-#                 try:
-#                     intort = [char2int[t] for t in ort]
-#                     encoded.append(intort)
-#                 except:
-#                     pass
-#             return encoded
-
-#         encoded = ort2int(total)
-
-#         x_padded = []
-#         for ort in encoded:
-#             seq = np.zeros(self.max_len)
-#             i = 0
-#             for _int in ort:
-#                seq[i] = _int
-#                i += 1
-#                x_padded.append(np.copy(seq))
-
-#         self.input_size = len(x_padded[0])
-
-#         self.X_tensors = [torch.LongTensor(seq) for seq in x_padded]
-#         self.y_tensors = torch.LongTensor([0 if ort in smaort else 1 for ort in total])
-
-#     def __len__(self):
-#         return self.len
-
-#     def __getitem__(self, i):
-#         return self.X_tensors[i].to(dev), self.y_tensors[i].to(dev)
-
-def train_model(pretrained):
-    model = GRUclassifier(vocab_size, len(dataset.X_tensors[0]), 50, 2, dev, pretrained=False)
-    trained_model = trained_batches(model, 20, dev, train_loader=train_loader, loss_mode=1)
+# def train_model(pretrained):
+#     model = GRUclassifier(vocab_size, len(dataset.X_tensors[0]), 50, 2, dev, pretrained=False)
+#     trained_model = trained_batches(model, 20, dev, train_loader=train_loader, loss_mode=1)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train a place name classifier.')
     parser.add_argument('--modelfile', type=str, default="trained_model",
                     help='The name of the file you wish to save the trained model to.')
     parser.add_argument('--dataset', type=str, default='default_dataset',
-                    help='The name of the file you wish to save the dataset to.')
+                    help='Path you wish to save the dataset to.')
     parser.add_argument('--pretrained', type=bool, default=False,
                     help='Whether or not to use pretrained weights.')
 
     args = parser.parse_args()
 
     dev = torch.device("cuda:{}".format(hash('gusstrlip') % 4) if torch.cuda.is_available() else "cpu")
+    
     smaort_excel, tatort_excel, concat_df, smaort_train, tatort_train = read_data()
     vocab, vocab_size = get_vocab(concat_df, 'Distriktsnamn')
     max_len = max(map(lambda x: len(x), concat_df['Distriktsnamn']))
     print('Max len:', max_len)
 
-    dataset = OrtLoader(smaort_train, tatort_train, vocab, max_len, dev, sammansattningar=None)
+    dataset = OrtLoader(smaort_train, tatort_train, vocab, max_len, dev, orter=None)
     train_loader = DataLoader(dataset=dataset, batch_size=32, shuffle=True, num_workers=0)
 
-    print('Saving dataset to {}'.format(args.dataset))
-    with open(args.dataset, 'wb') as f:
+    datapath = 'datasets/' + args.dataset
+    print('Saving dataset to {}'.format(datapath))
+    with open(datapath, 'wb') as f:
         pickle.dump(dataset, f)
 
     model = GRUclassifier(vocab_size, len(dataset.X_tensors[0]), 50, 2, dev, args.pretrained)
     trained_model = trained_batches(model, 20, dev, train_loader=train_loader, loss_mode=1)
-    filename = args.modelfile
-    print('Saving model to {}'.format(filename))
-    torch.save(trained_model, filename)
+    #filename = args.modelfile
+    filepath = 'trained_models/' + args.modelfile
+    #print('Saving model to {}'.format(filename))
+    print('Saving model to {}'.format(filepath))
+    #torch.save(trained_model, filename)
+    torch.save(trained_model, filepath)
