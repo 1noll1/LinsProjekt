2dea9b76885db7c6a0c1ade321ce7027d47473ba
Linnea Strand <gusstrlip@student.gu.se>
2020-12-22 20:57:03 +0100

    ...

--- GRU_model.py
+++ GRU_model.py
@@ -1,6 +1,8 @@
 import torch
-from torch import optim
 from torch import nn
+from transformers import AutoModel
+bert_model = AutoModel.from_pretrained('KB/bert-base-swedish-cased')
+
 
 class GRUclassifier(nn.Module):
     def __init__(self, vocab_size, input_size, hidden_size, output_size, dev, pretrained):
@@ -8,29 +10,29 @@ class GRUclassifier(nn.Module):
         self.dev = dev
         self.num_layers = 1
         self.hidden_size = hidden_size
-        if pretrained is not False:
+        if pretrained is not None:
             print('Using pretrained FastText weights')
             weight = pretrained
             self.embed = nn.Embedding.from_pretrained(weight)
-        if pretrained is False:
+        if pretrained is None:
             print('Using PyTorch embeddings layer')
             self.embed = nn.Embedding(vocab_size, input_size)
-        self.gru = nn.GRU(input_size, hidden_size, num_layers=1, batch_first=True)
-        self.linear = nn.Linear(input_size * hidden_size, output_size) 
+        self.gru = nn.GRU(768, hidden_size, num_layers=1, batch_first=True)
+        self.output_size = output_size
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        output = self.embed(x)
-        h = self.init_hidden(len(x))     
+        output, _ = bert_model(x)
+        # output = self.embed(x)
+        h = self.init_hidden(len(x))
         output, hidden = self.gru(output, h)
-        output = output.contiguous().view(-1, self.hidden_size * len(x[0])) # -1 just infers the size
-        output = self.linear(output) 
-        #print(output.shape)
+        output = output.contiguous().view(-1, self.hidden_size * len(x[0]))  # -1 just infers the size
+        linear_layer = nn.Linear(len(x[0]) * self.hidden_size, self.output_size)
+        output = linear_layer(output)
         return self.sigmoid(output)
-        #return self.logsoftmax(output)
-    
+
     def set_dev(self, dev):
         self.dev = dev
-    
+
     def init_hidden(self, x_len):
         return torch.zeros(self.num_layers, x_len, self.hidden_size).to(self.dev)
--- OrtLoader.py
+++ OrtLoader.py
@@ -1,50 +1,45 @@
-import numpy as np
 import torch
+from transformers import AutoTokenizer
 
 class OrtLoader():
     def __init__(self, smaort, tatort, vocab, max_len, dev, orter=None):
 
-        if orter == None:
+        tok = AutoTokenizer.from_pretrained('KB/bert-base-swedish-cased')
+
+        if orter is None:
             total = smaort + tatort
-        if orter != None:
+        if orter is not None:
             total = orter.values()
 
         self.dev = dev
         self.max_len = max_len
         self.vocab = vocab
-        self.char2int = {t:n for n, t in enumerate(vocab)}
+        self.char2int = {t: n for n, t in enumerate(vocab)}
 
         self.len = len(total)
 
-        def ort2int(total):   
-            encoded = []         
-            for i, ort in enumerate(total):
+
+        def ort2int(all_placenames):
+            encoded_placenames = []
+            for i, ort in enumerate(all_placenames):
                 try:
-                    intort = [self.char2int[t] for t in ort]
-                    encoded.append(intort)
+                    intort = tok.encode(ort)
+                    # intort = [self.char2int[t] for t in ort]
+                    encoded_placenames.append(intort)
                 except:
-                    pass
-            return encoded
+                    print('Encoding of placename {} failed'.format(ort))
+            return encoded_placenames
 
         encoded = ort2int(total)
 
-        print('Padding vectors up to max len: {}'.format(max_len))
-        x_padded = []
-        for ort in encoded:
-            seq = np.zeros(self.max_len)
-            i = 0
-            for _int in ort:
-               seq[i] = _int
-               i += 1
-               x_padded.append(np.copy(seq))
+        self.X_tensors = [torch.LongTensor(x) for x in encoded]
 
-        self.input_size = len(x_padded[0])
+        self.input_size = max_len
 
-        self.X_tensors = [torch.LongTensor(seq) for seq in x_padded]
-        if orter == None:
-            self.y_tensors = torch.FloatTensor([0 if ort in smaort else 1 for ort in total])
-        if orter != None:
-            self.y_tensors = torch.FloatTensor([0 if ort in smaort else 1 for ort in orter.keys()])
+        if orter is None:
+            self.y_tensors = torch.Tensor([0 if ort in smaort else 1 for ort in total])
+        if orter is not None:
+            self.y_tensors = torch.Tensor([0 if ort in smaort else 1 for ort in orter.keys()])
 
     def __len__(self):
         return self.len
